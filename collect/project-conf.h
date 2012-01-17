d0999130f77dbf7eea143ba506568c0e019be2de
m0ep <email.mufl@googlemail.com>
2012-01-17 18:18:02 +0100

    collect stub

---
+++ collect/project-conf.h
@@ -0,0 +1,6 @@
+// specify new RDC (Radio Duty Cycling) and MAC settings
+#define NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE 8
+#define NETSTACK_CONF_RDC xmac_driver
+#define NETSTACK_CONF_MAC csma_driver
+#define RF_CHANNEL 26
+
