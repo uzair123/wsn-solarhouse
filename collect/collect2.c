f6eaebc2ea6267e711e1d14f333c172bd3c192d1
wsn lab <wsn@mint>
2012-01-18 12:37:06 +0100

    collect 2 newest version, DONT DELETE!

--- collect/collect2.c
+++ collect/collect2.c
@@ -1,99 +1,110 @@
 #include "contiki.h"
 #include "lib/random.h"
+
 #include "net/rime.h"
 #include "net/rime/collect.h"
-#include "net/netstack.h"
+
+#include "dev/battery-sensor.h"
+#include "dev/humidity-sensor.h"
+#include "dev/co-sensor.h"
+#include "dev/sensorstation.h"
+#include "dev/tmp102.h"
+#include "dev/cc2420.h"
+
 #include <stdio.h>
-
-/**
-		modified a bit, didn't want to change the old file
-*/
-
+#include <stdlib.h>
+#include <string.h>
+/*---------------------------------------------------------------------------*/
 //#define SINK 1
+
 #define SEND_INTERVAL_SECONDS 30
+#define SETTLE_TIMEOUT_SECONDS 120
 #define COLLECT_CHANNEL 130
-
-static struct collect_conn tc;
-
-//is it a difference if it is static?
-struct collect_data{
-    int light;
-    uint8_t temp;
-    int co2;
-    int humidity;
-}
-
+#define NUM_RETRANSMITS 15
+// we use maximum transmission power to compare it with the final implementation
+#define CC2420_TXPOWER 31
 /*---------------------------------------------------------------------------*/
 PROCESS(solarhouse_collect_process, "Solarhouse Collect");
 AUTOSTART_PROCESSES(&solarhouse_collect_process);
 /*---------------------------------------------------------------------------*/
-static void
-recv(const rimeaddr_t *originator, uint8_t seqno, uint8_t hops){
-	
-	//pointer: daten is already on the mote
-	struct collect_data *data = packetbuf_dataptr();
+static struct collect_conn tc;
+/*---------------------------------------------------------------------------*/
+struct solarhouse_sensor_data{
+    int16_t co2_station;
+    int16_t temp_station;
+    int16_t humidity_station;
+    int16_t temp_z1;
+    int16_t humidity_z1;
+    int16_t battery;
+};
+/*---------------------------------------------------------------------------*/
+static struct solarhouse_sensor_data read_sensors();
+/*---------------------------------------------------------------------------*/
+static void recv(const rimeaddr_t *originator, uint8_t seqno, uint8_t hops){
 
-	printf(""Sink got message from %d.%d, seqno %d, hops %d: len %d :: light: %d temp: %u co2: %d humidity: %d\n", 
-		originator->u8[0], originator->u8[1],
-		seqno, hops,
-		packetbuf_datalen(),		
-		data->light, 
-		data->temp, 
-		data->co2, 
-		data->humidity);
+	//data is already on the mote, copying of the data is not necessary
+	struct solarhouse_sensor_data *data = packetbuf_dataptr();
+
+	printf("light: %d temperature: %d co2: %d humidity: %d battery: %d\n", 
+	data.light, 
+	data.temperature, 
+	data.co2, 
+	data.humidity,
+	data.battery);
 }
 /*---------------------------------------------------------------------------*/
 static const struct collect_callbacks callbacks = { recv };
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(solarhouse_collect_process, ev, data){
-
-	static struct etimer periodic;
-	static struct etimer et;
+    static struct etimer periodic;
+    static struct etimer et;
   
-	PROCESS_BEGIN();
+    PROCESS_BEGIN();
 
-	collect_open(&tc, COLLECT_CHANNEL, COLLECT_ROUTER, &callbacks);
+    cc2420_set_txpower(CC2420_TXPOWER);
 
-	#ifdef SINK
-		printf("i'am sink\n");
-		collect_set_sink(&tc, 1);
-	#endif 
+    /* initialize sensors */
+    SENSORS_ACTIVATE(battery_sensor);
+    /* ... */
+
+    collect_open(&tc, COLLECT_CHANNEL, COLLECT_ROUTER, &callbacks);
+
+#ifdef SINK
+    printf("i'am sink\n");
+    collect_set_sink(&tc, 1);
+#endif
  
-	/* Allow some time for the network to settle. */
-	etimer_set(&et, 120 * CLOCK_SECOND);
-	PROCESS_WAIT_UNTIL(etimer_expired(&et));
+    /* Allow some time for the network to settle. */
+    etimer_set(&et, SETTLE_TIMEOUT_SECONDS * CLOCK_SECOND);
+    PROCESS_WAIT_UNTIL(etimer_expired(&et));
 
-	while(1) {
+    while(1) {
+        /* Send a packet every SEND_INTERVAL_SECONDS seconds. */
+        if(etimer_expired(&periodic)) {
+            etimer_set(&periodic, CLOCK_SECOND * SEND_INTERVAL_SECONDS);
+            etimer_set(&et, random_rand() % (CLOCK_SECOND * SEND_INTERVAL_SECONDS));
+        }
 
-		/* Send a packet every SEND_INTERVAL_SECONDS seconds. */
-		if(etimer_expired(&periodic)) {
-			etimer_set(&periodic, CLOCK_SECOND * SEND_INTERVAL_SECONDS);
-			etimer_set(&et, random_rand() % (CLOCK_SECOND * SEND_INTERVAL_SECONDS));
-		}
+        PROCESS_WAIT_EVENT();
 
-		PROCESS_WAIT_EVENT();
-
-		#ifndef SINK
-			// send sensor data
-			if(etimer_expired(&et)) {
-			// TODO read sensors
-			collect_send(&tc, 15);
-			}
-		#endif
-	}
-	PROCESS_END();
+#ifndef SINK
+        // send sensor data
+        if(etimer_expired(&et)) {
+            struct solarhouse_sensor_data data = read_sensors();
+            // TODO read sensors
+            collect_send(&tc, NUM_RETRANSMITS);
+        }
+#endif
+    }
+    
+    PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
-
-
-
-
-
-
-
-
-
-
-
-
-
+static struct solarhouse_sensor_data read_sensors(){
+    struct solarhouse_sensor_data data;
+    memset(&data, 0, sizeof(struct solarhouse_sensor_data));
+    
+    data.battery = battery_sensor.value(0);
+    
+    return data;
+}
