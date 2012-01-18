c2ce0e0762154125ab07e48d5fba31cebab281b6
wsn lab <wsn@mint>
2012-01-18 15:38:07 +0100

    collect compile

--- HelloSensors/hello2.c
+++ HelloSensors/hello2.c
@@ -1,7 +1,10 @@
 #include "contiki.h"
 #include <stdio.h>
-#include "dev/sensorstation.h"
 #include "sensors.h"
+#include "dev/tmp102.h"
+
+
+#define LIGHT_SENSOR_TOTAL_SOLAR 1
 
 /*----------------------------------------------------------------*/
 PROCESS(sensor_test_process, "sensor_test_process");
@@ -9,23 +12,23 @@ AUTOSTART_PROCESSES(&sensor_test_process);
 /*----------------------------------------------------------------*/
 PROCESS_THREAD(sensor_test_process, ev, data)
 {
-	struct sensor_values results;
+	static uint16_t temperature, light, co2, humidity;
 	static struct etimer et;
+	static int8_t temp2;
 
 	PROCESS_BEGIN();
-	//SENSORS_ACTIVATE(temperature_sensor);
-	//SENSORS_ACTIVATE(humidity_sensor);
-	sensorstation_init();
-
+	
+	tmp102_init();
+	
 	while(1) { 
+		// 30 seconds timer
 		etimer_set(&et, CLOCK_SECOND * 1);
 		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
-		results = sensorstation_read();
-		//temperature = temperature_sensor.value(0);
-		//humidity = humidity_sensor.value(0);
+		temp2 = tmp102_read_temp_simple();		
 		
-		printf("temperature: %d C/1000 , humidity: %d%%/1000\r\n", results.temperature, results.humidity);
+		printf("temperature: %d \r\n", 
+			temp2);
 	}
 	PROCESS_END();
 }
--- test/collect2.c
+++ test/collect2.c
@@ -1,27 +1,27 @@
 #include "contiki.h"
-//#include "lib/random.h"
+#include "lib/random.h"
 
-//#include "net/rime.h"
+#include "net/rime.h"
 #include "net/rime/collect.h"
 
-//#include "dev/battery-sensor.h"
-//#include "dev/humidity-sensor.h"
-//#include "dev/tmp102.h"
-//#include "dev/co-sensor.h"
-//#include "dev/sensorstation.h"
-//#include "sensors.h"
+#include "dev/battery-sensor.h"
+#include "dev/humidity-sensor.h"
+#include "dev/tmp102.h"
+#include "dev/sensorstation.h"
+#include "sensors.h"
 
-//#include "dev/cc2420.h"
-//#include <stdio.h>
-//include <stdlib.h>
-//#include <string.h>
+#include "dev/cc2420.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /*---------------------------------------------------------------------------*/
-/*#define SEND_INTERVAL_SECONDS 30
+#define SINK 1
+#define SEND_INTERVAL_SECONDS 30
 #define SETTLE_TIMEOUT_SECONDS 120
-#define COLLECT_CHANNEL_x 130
-#define NUM_RETRANSMITS_x 15
+#define COLLECT_CHANNEL 130
+#define NUM_RETRANSMITS 15
 // we use maximum transmission power to compare it with the final implementation
-#define CC2420_TXPOWER_x 31*/
+#define CC2420_TXPOWER 31
 /*---------------------------------------------------------------------------*/
 PROCESS(solarhouse_collect_process, "Solarhouse Collect");
 AUTOSTART_PROCESSES(&solarhouse_collect_process);
@@ -45,12 +45,11 @@ static void recv(const rimeaddr_t *originator, uint8_t seqno, uint8_t hops){
 	//data is already on the mote, copying of the data is not necessary
 	struct solarhouse_sensor_data *data = packetbuf_dataptr();
 
-	printf("light: %d temperature: %d co2: %d humidity: %d battery: %d\n", 
-	data.light, 
-	data.temperature, 
-	data.co2, 
-	data.humidity,
-	data.battery);
+	printf("temperature: %d co2: %d humidity: %d battery: %d\n", 
+	data->temp_z1, 
+	data->co2_station, 
+	data->humidity_z1,
+	data->battery);
 }
 /*---------------------------------------------------------------------------*/
 static const struct collect_callbacks callbacks = { recv };
@@ -61,9 +60,9 @@ PROCESS_THREAD(solarhouse_collect_process, ev, data){
   
     PROCESS_BEGIN();
 
-    //cc2420_set_txpower(CC2420_TXPOWER_x);
+    cc2420_set_txpower(CC2420_TXPOWER);
 
-    /* initialize sensors 
+    /* initialize sensors */
 		// DEPENDING ON WHICH DEVICE!! or not?
 	SENSORS_ACTIVATE(battery_sensor);
 	SENSORS_ACTIVATE(humidity_sensor);
@@ -71,7 +70,7 @@ PROCESS_THREAD(solarhouse_collect_process, ev, data){
 	tmp102_init();
     /* ... */
 
-    collect_open(&tc, COLLECT_CHANNEL_x, COLLECT_ROUTER, &callbacks);
+    collect_open(&tc, COLLECT_CHANNEL, COLLECT_ROUTER, &callbacks);
 
 #ifdef SINK
     printf("i'am sink\n");
@@ -94,9 +93,10 @@ PROCESS_THREAD(solarhouse_collect_process, ev, data){
 #ifndef SINK
         // send sensor data
         if(etimer_expired(&et)) {
-            //struct solarhouse_sensor_data data2 = read_sensors();
-            // TODO read sensors
-            collect_send(&tc, NUM_RETRANSMITS_x);
+            	struct solarhouse_sensor_data data = read_sensors();
+		packetbuf_clear();
+		packetbuf_copyfrom(&data, sizeof(struct solarhouse_sensor_data));
+            collect_send(&tc, NUM_RETRANSMITS);
         }
 #endif
     }
@@ -105,7 +105,7 @@ PROCESS_THREAD(solarhouse_collect_process, ev, data){
 }
 /*---------------------------------------------------------------------------*/
 static struct solarhouse_sensor_data read_sensors(){
-/*
+
 	struct solarhouse_sensor_data data;
 	memset(&data, 0, sizeof(struct solarhouse_sensor_data));
 
@@ -117,5 +117,5 @@ static struct solarhouse_sensor_data read_sensors(){
 	data.humidity_z1 = humidity_sensor.value(0);
 	data.battery = battery_sensor.value(0);
     
-	return data;*/
+	return data;
 }
