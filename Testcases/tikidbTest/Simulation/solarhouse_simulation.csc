5e20d77e9d028512a938675a26f881c95dd96711
Thomas Lack <post.thomas.lack@googlemail.com>
2012-03-07 12:59:31 +0100

    * 3 scopes added to solarhouse version of tikidb tester * simulation file with matching node id's (according to those used in the solarhouse)

---
+++ Testcases/tikidbTest/Simulation/solarhouse_simulation.csc
@@ -0,0 +1,216 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<simconf>
+  <project EXPORT="discard">[CONTIKI_DIR]/tools/cooja/apps/mrm</project>
+  <project EXPORT="discard">[CONTIKI_DIR]/tools/cooja/apps/mspsim</project>
+  <project EXPORT="discard">[CONTIKI_DIR]/tools/cooja/apps/avrora</project>
+  <project EXPORT="discard">[CONTIKI_DIR]/tools/cooja/apps/serial_socket</project>
+  <project EXPORT="discard">[CONTIKI_DIR]/tools/cooja/apps/collect-view</project>
+  <simulation>
+    <title>tikidb solarhouse simulation</title>
+    <delaytime>0</delaytime>
+    <randomseed>123456</randomseed>
+    <motedelay_us>1000000</motedelay_us>
+    <radiomedium>
+      se.sics.cooja.radiomediums.UDGM
+      <transmitting_range>50.0</transmitting_range>
+      <interference_range>100.0</interference_range>
+      <success_ratio_tx>1.0</success_ratio_tx>
+      <success_ratio_rx>1.0</success_ratio_rx>
+    </radiomedium>
+    <events>
+      <logoutput>40000</logoutput>
+    </events>
+    <motetype>
+      se.sics.cooja.mspmote.SkyMoteType
+      <identifier>sky1</identifier>
+      <description>Sky Mote Type #sky1</description>
+      <firmware EXPORT="copy">/home/lack/DEV/workspace/tikidb/tikidb-collect.sky</firmware>
+      <moteinterface>se.sics.cooja.interfaces.Position</moteinterface>
+      <moteinterface>se.sics.cooja.interfaces.RimeAddress</moteinterface>
+      <moteinterface>se.sics.cooja.interfaces.IPAddress</moteinterface>
+      <moteinterface>se.sics.cooja.interfaces.Mote2MoteRelations</moteinterface>
+      <moteinterface>se.sics.cooja.interfaces.MoteAttributes</moteinterface>
+      <moteinterface>se.sics.cooja.mspmote.interfaces.MspClock</moteinterface>
+      <moteinterface>se.sics.cooja.mspmote.interfaces.MspMoteID</moteinterface>
+      <moteinterface>se.sics.cooja.mspmote.interfaces.SkyButton</moteinterface>
+      <moteinterface>se.sics.cooja.mspmote.interfaces.SkyFlash</moteinterface>
+      <moteinterface>se.sics.cooja.mspmote.interfaces.SkyCoffeeFilesystem</moteinterface>
+      <moteinterface>se.sics.cooja.mspmote.interfaces.SkyByteRadio</moteinterface>
+      <moteinterface>se.sics.cooja.mspmote.interfaces.MspSerial</moteinterface>
+      <moteinterface>se.sics.cooja.mspmote.interfaces.SkyLED</moteinterface>
+      <moteinterface>se.sics.cooja.mspmote.interfaces.MspDebugOutput</moteinterface>
+      <moteinterface>se.sics.cooja.mspmote.interfaces.SkyTemperature</moteinterface>
+    </motetype>
+    <mote>
+      <breakpoints />
+      <interface_config>
+        se.sics.cooja.interfaces.Position
+        <x>29.36468036227026</x>
+        <y>39.315623878754494</y>
+        <z>0.0</z>
+      </interface_config>
+      <interface_config>
+        se.sics.cooja.mspmote.interfaces.MspMoteID
+        <id>53</id>
+      </interface_config>
+      <motetype_identifier>sky1</motetype_identifier>
+    </mote>
+    <mote>
+      <breakpoints />
+      <interface_config>
+        se.sics.cooja.interfaces.Position
+        <x>48.11339675554696</x>
+        <y>34.23784652224205</y>
+        <z>0.0</z>
+      </interface_config>
+      <interface_config>
+        se.sics.cooja.mspmote.interfaces.MspMoteID
+        <id>51</id>
+      </interface_config>
+      <motetype_identifier>sky1</motetype_identifier>
+    </mote>
+    <mote>
+      <breakpoints />
+      <interface_config>
+        se.sics.cooja.interfaces.Position
+        <x>8.272374419833978</x>
+        <y>10.020754514259657</y>
+        <z>0.0</z>
+      </interface_config>
+      <interface_config>
+        se.sics.cooja.mspmote.interfaces.MspMoteID
+        <id>56</id>
+      </interface_config>
+      <motetype_identifier>sky1</motetype_identifier>
+    </mote>
+    <mote>
+      <breakpoints />
+      <interface_config>
+        se.sics.cooja.interfaces.Position
+        <x>27.411689071303936</x>
+        <y>10.020754514259657</y>
+        <z>0.0</z>
+      </interface_config>
+      <interface_config>
+        se.sics.cooja.mspmote.interfaces.MspMoteID
+        <id>69</id>
+      </interface_config>
+      <motetype_identifier>sky1</motetype_identifier>
+    </mote>
+    <mote>
+      <breakpoints />
+      <interface_config>
+        se.sics.cooja.interfaces.Position
+        <x>47.33220023916043</x>
+        <y>10.020754514259657</y>
+        <z>0.0</z>
+      </interface_config>
+      <interface_config>
+        se.sics.cooja.mspmote.interfaces.MspMoteID
+        <id>60</id>
+      </interface_config>
+      <motetype_identifier>sky1</motetype_identifier>
+    </mote>
+    <mote>
+      <breakpoints />
+      <interface_config>
+        se.sics.cooja.interfaces.Position
+        <x>79.36125741100811</x>
+        <y>35.01904303862858</y>
+        <z>0.0</z>
+      </interface_config>
+      <interface_config>
+        se.sics.cooja.mspmote.interfaces.MspMoteID
+        <id>63</id>
+      </interface_config>
+      <motetype_identifier>sky1</motetype_identifier>
+    </mote>
+    <mote>
+      <breakpoints />
+      <interface_config>
+        se.sics.cooja.interfaces.Position
+        <x>79.75185566920138</x>
+        <y>23.30109529283065</y>
+        <z>0.0</z>
+      </interface_config>
+      <interface_config>
+        se.sics.cooja.mspmote.interfaces.MspMoteID
+        <id>64</id>
+      </interface_config>
+      <motetype_identifier>sky1</motetype_identifier>
+    </mote>
+    <mote>
+      <breakpoints />
+      <interface_config>
+        se.sics.cooja.interfaces.Position
+        <x>80.14245392739464</x>
+        <y>12.754942321612509</y>
+        <z>0.0</z>
+      </interface_config>
+      <interface_config>
+        se.sics.cooja.mspmote.interfaces.MspMoteID
+        <id>65</id>
+      </interface_config>
+      <motetype_identifier>sky1</motetype_identifier>
+    </mote>
+  </simulation>
+  <plugin>
+    se.sics.cooja.plugins.SimControl
+    <width>290</width>
+    <z>1</z>
+    <height>192</height>
+    <location_x>0</location_x>
+    <location_y>0</location_y>
+  </plugin>
+  <plugin>
+    se.sics.cooja.plugins.Visualizer
+    <plugin_config>
+      <skin>se.sics.cooja.plugins.skins.LEDVisualizerSkin</skin>
+      <skin>se.sics.cooja.plugins.skins.IDVisualizerSkin</skin>
+      <skin>se.sics.cooja.plugins.skins.TrafficVisualizerSkin</skin>
+      <skin>se.sics.cooja.plugins.skins.GridVisualizerSkin</skin>
+      <skin>se.sics.cooja.plugins.skins.UDGMVisualizerSkin</skin>
+      <viewport>2.560175267103237 0.0 0.0 2.560175267103237 100.82127161012356 91.34511213487932</viewport>
+    </plugin_config>
+    <width>791</width>
+    <z>0</z>
+    <height>457</height>
+    <location_x>809</location_x>
+    <location_y>0</location_y>
+  </plugin>
+  <plugin>
+    se.sics.cooja.plugins.LogListener
+    <plugin_config>
+      <filter />
+    </plugin_config>
+    <width>1600</width>
+    <z>2</z>
+    <height>218</height>
+    <location_x>0</location_x>
+    <location_y>463</location_y>
+  </plugin>
+  <plugin>
+    se.sics.cooja.plugins.TimeLine
+    <plugin_config>
+      <mote>0</mote>
+      <mote>1</mote>
+      <mote>2</mote>
+      <mote>3</mote>
+      <mote>4</mote>
+      <mote>5</mote>
+      <mote>6</mote>
+      <mote>7</mote>
+      <showRadioRXTX />
+      <showRadioHW />
+      <showLEDs />
+      <split>118</split>
+      <zoomfactor>500.0</zoomfactor>
+    </plugin_config>
+    <width>1600</width>
+    <z>3</z>
+    <height>150</height>
+    <location_x>0</location_x>
+    <location_y>681</location_y>
+  </plugin>
+</simconf>
+
--- Testcases/tikidbTest/tikidb-collect.c
+++ Testcases/tikidbTest/tikidb-collect.c
@@ -11,26 +11,29 @@
 #include "scopes-membership-simple.h"
 #include "scopes-repository.h"
 // sensor drivers according to tikidb wiki
-#include "dev/light-sensor.h"
-#include "dev/humidity-sensor.h"
-#include "dev/tmp102.h"
-#include "dev/sensorstation.h"
+//#include "dev/light-sensor.h"
+//#include "dev/humidity-sensor.h"
+//#include "dev/tmp102.h"
+//#include "dev/sensorstation.h"
 
 
 PROCESS(tikidb_collect_process, "tikidb collect");
 AUTOSTART_PROCESSES(&tikidb_collect_process);
 
 static bool keep_running;
-#define SCOPE_ID 1
-// sample period is 1-minute==60 seconds
-#define SAMPLE_PERIOD_VALUE_L (CLOCK_SECOND*60)
+// defining which scopes we want to use in the solar house
+#define SCOPE_ID_INSIDE_UPPER 1
+#define SCOPE_ID_INSIDE_LOWER 2
+#define SCOPE_ID_OUTSIDE 3
+// sample period is 1-minute==30 seconds
+#define SAMPLE_PERIOD_VALUE_L (CLOCK_SECOND*30)
 #define SAMPLE_PERIOD_VALUE_1 (SAMPLE_PERIOD_VALUE_L/(256UL*256*256))%256
 #define SAMPLE_PERIOD_VALUE_2 (SAMPLE_PERIOD_VALUE_L/(256UL*256))%256
 #define SAMPLE_PERIOD_VALUE_3 (SAMPLE_PERIOD_VALUE_L/256)%256
 #define SAMPLE_PERIOD_VALUE_4 (SAMPLE_PERIOD_VALUE_L)%256
 
-// lifetime is 5*256+160 samples == 1440 samples == 1440 * 60seconds == 1440* 1 minutes == 24*60 minutes == 24hours
-#define LIFETIME_VALUE_L ((CLOCK_SECOND*60)*60*24)				//        11,059,200
+// lifetime is 5*256+160 samples == 1440 samples == 1440 * 60seconds == 1440* 1 minutes == 24*60*7 minutes == 24*7 hours = 7 days
+#define LIFETIME_VALUE_L ((CLOCK_SECOND*60)*60*24*7)			//        11,059,200
 #define LIFETIME_VALUE_1 (LIFETIME_VALUE_L/(256UL*256*256))%256	//   0 ==          0
 #define LIFETIME_VALUE_2 (LIFETIME_VALUE_L/(256*256UL))%256		// 168 == 11,010,048
 #define LIFETIME_VALUE_3 (LIFETIME_VALUE_L/256)%256				// 192 ==     49,152
@@ -81,7 +84,6 @@ void cb_shell_tikidb_result(uint8_t* result_dump, select_statement_t *ss) {
 				clock_time(), __FILE__, __FUNCTION__);
 
 }
-
 /*----------------------------------------------------------------------------*/
 PROCESS_THREAD(tikidb_collect_process, ev, data) {
 	PROCESS_BEGIN();
@@ -92,43 +94,93 @@ PROCESS_THREAD(tikidb_collect_process, ev, data) {
 	/* wait a while */
 	etimer_set(&control_timer, 10 * CLOCK_SECOND);
 	PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&control_timer));
-	PRINTF(1,"(TIKIDB TESTER) tester application started\n");
-
-	// init tikidb and sensors
-	// TODO: sensor abfragen in tikidb selber einbauen
-	tikidb_init(cb_shell_tikidb_result);
-	light_sensor.configure(SENSORS_ACTIVE,1);
-	humidity_sensor.configure(SENSORS_ACTIVE,1);
-	tmp102_init();
-	// sensorstation_init(); // undefined reference to 'uart1_init' and 'uart1_set_input'
+	PRINTF(1,"(TIKIDB SOLARHOUSE TESTER) tester application started\n");
 
 	// TODO: nodes dürfen den scope nicht verlassen
 
 	// sink node control
-	if (node_id == 1) {
-		static uint8_t scope_statement_arr[] = { //
+	if (node_id == 53) {
+		// open scope for nodes inside the solar house (in the ceiling)
+		static uint8_t scope_statement_upper_arr[] = { //
 			STATEMENT, // indicates that the byte array is a statement (instead of a result)
 			11, // statement id
 			OPEN_SCOPE_STATEMENT, // type of statement is OPEN SCOPE (instead e.g., of STOP or LIST_QUERIES)
-			SCOPES_WORLD_SCOPE_ID, // if of super scope
-			SCOPE_ID, // id of the scope
+			SCOPES_WORLD_SCOPE_ID, // id of super scope
+			SCOPE_ID_INSIDE_UPPER, // id of the scope
 			0, // TTL
 			40, //
-			5, // scope spec length
-			PREDICATE_GT, //
-			REPOSITORY_VALUE, //
-			NODE_ID, //
-			UINT8_VALUE, //
-			2 };
+			35, // scope spec length
+			OPERATOR_OR,
+			PREDICATE_EQ, REPOSITORY_VALUE, NODE_ID, UINT8_VALUE, 52,
+			OPERATOR_OR,
+			PREDICATE_EQ, REPOSITORY_VALUE, NODE_ID, UINT8_VALUE, 56,
+			OPERATOR_OR,
+			PREDICATE_EQ, REPOSITORY_VALUE, NODE_ID, UINT8_VALUE, 57,
+			OPERATOR_OR,
+			PREDICATE_EQ, REPOSITORY_VALUE, NODE_ID, UINT8_VALUE, 58,
+			OPERATOR_OR,
+			PREDICATE_EQ, REPOSITORY_VALUE, NODE_ID, UINT8_VALUE, 60,
+			PREDICATE_EQ, REPOSITORY_VALUE, NODE_ID, UINT8_VALUE, 69
+		};
 
-		PRINTF(1,"(TIKIDB TESTER) Opening scope %u\n", SCOPE_ID);
-		tikidb_process_statement(scope_statement_arr,
-				sizeof(scope_statement_arr));
+		PRINTF(1,"(TIKIDB SOLARHOUSE TESTER) Opening scope %u\n", SCOPE_ID_INSIDE_UPPER);
+				tikidb_process_statement(scope_statement_upper_arr,
+						sizeof(scope_statement_upper_arr));
+
+		// open scope for nodes inside the solar house (on the floor)
+		static uint8_t scope_statement_lower_arr[] = { //
+			STATEMENT, // indicates that the byte array is a statement (instead of a result)
+			11, // statement id
+			OPEN_SCOPE_STATEMENT, // type of statement is OPEN SCOPE (instead e.g., of STOP or LIST_QUERIES)
+			SCOPES_WORLD_SCOPE_ID, // id of super scope
+			SCOPE_ID_INSIDE_LOWER, // id of the scope
+			0, // TTL
+			40, //
+			23, // scope spec length
+			OPERATOR_OR,
+			PREDICATE_EQ, REPOSITORY_VALUE, NODE_ID, UINT8_VALUE, 51,
+			OPERATOR_OR,
+			PREDICATE_EQ, REPOSITORY_VALUE, NODE_ID, UINT8_VALUE, 53,
+			OPERATOR_OR,
+			PREDICATE_EQ, REPOSITORY_VALUE, NODE_ID, UINT8_VALUE, 54,
+			PREDICATE_EQ, REPOSITORY_VALUE, NODE_ID, UINT8_VALUE, 55
+		};
+
+		PRINTF(1,"(TIKIDB SOLARHOUSE TESTER) Opening scope %u\n", SCOPE_ID_INSIDE_LOWER);
+				tikidb_process_statement(scope_statement_lower_arr,
+						sizeof(scope_statement_lower_arr));
+
+		// open scope for nodes outside the solar house
+		static uint8_t scope_statement_outer_arr[] = { //
+			STATEMENT, // indicates that the byte array is a statement (instead of a result)
+			11, // statement id
+			OPEN_SCOPE_STATEMENT, // type of statement is OPEN SCOPE (instead e.g., of STOP or LIST_QUERIES)
+			SCOPES_WORLD_SCOPE_ID, // id of super scope
+			SCOPE_ID_OUTSIDE, // id of the scope
+			0, // TTL
+			40, //
+			35, // scope spec length
+			OPERATOR_OR,
+			PREDICATE_EQ, REPOSITORY_VALUE, NODE_ID, UINT8_VALUE, 63,
+			OPERATOR_OR,
+			PREDICATE_EQ, REPOSITORY_VALUE, NODE_ID, UINT8_VALUE, 64,
+			OPERATOR_OR,
+			PREDICATE_EQ, REPOSITORY_VALUE, NODE_ID, UINT8_VALUE, 65,
+			OPERATOR_OR,
+			PREDICATE_EQ, REPOSITORY_VALUE, NODE_ID, UINT8_VALUE, 66,
+			OPERATOR_OR,
+			PREDICATE_EQ, REPOSITORY_VALUE, NODE_ID, UINT8_VALUE, 67,
+			PREDICATE_EQ, REPOSITORY_VALUE, NODE_ID, UINT8_VALUE, 68
+		};
+
+		PRINTF(1,"(TIKIDB SOLARHOUSE TESTER) Opening scope %u\n", SCOPE_ID_OUTSIDE);
+		tikidb_process_statement(scope_statement_outer_arr,
+				sizeof(scope_statement_outer_arr));
 
 		/* wait a while */
 //		PRINTF(1,"(TIKIDB TESTER) Waiting 2.5 tree rebuild periods.\n");
 //		etimer_set(&control_timer, 2.5 * SELFUR_ROUTING_REFRESH_INTERVAL* CLOCK_SECOND);
-		PRINTF(1,"(TIKIDB TESTER) Waiting 1.1 tree rebuild periods\n");
+		PRINTF(1,"(TIKIDB SOLARHOUSE TESTER) Waiting 1.1 tree rebuild periods\n");
 				etimer_set(&control_timer, 1.1 * SELFUR_ROUTING_REFRESH_INTERVAL * CLOCK_SECOND);
 //		etimer_set(&control_timer, 2.5 * CLOCK_SECOND);
 		PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&control_timer));
@@ -146,7 +198,7 @@ PROCESS_THREAD(tikidb_collect_process, ev, data) {
 			NONE, NODE_RSSI, /* no aggregation, rssi */
 			NONE, NODE_PARENT, /* no aggregation, node parent */
 			1, /* number of scopes */
-			SCOPE_ID, /* id of the scope */
+			SCOPES_WORLD_SCOPE_ID, /* id of the scope */
 			// scope2_id (if number of scopes = 2)
 			// scope3_id (if number of scopes = 3)
 			// ...
@@ -173,15 +225,38 @@ PROCESS_THREAD(tikidb_collect_process, ev, data) {
 			PRINTF(1,"(TIKIDB TESTER) Waiting 10 seconds\n");
 		}
 
-		static uint8_t close_scope_statement_arr[] = { //
-				STATEMENT, // indicates that the byte array is a statement (instead of a result)
-				12, // statement id
-				CLOSE_SCOPE_STATEMENT, // type of statement is OPEN SCOPE (instead e.g., of STOP or LIST_QUERIES)
-				SCOPE_ID }; // id of the scope
+		static uint8_t close_scope_statement_upper_arr[] = { //
+			STATEMENT, // indicates that the byte array is a statement (instead of a result)
+			12, // statement id
+			CLOSE_SCOPE_STATEMENT, // type of statement is OPEN SCOPE (instead e.g., of STOP or LIST_QUERIES)
+			SCOPE_ID_INSIDE_UPPER // id of the scope
+		};
 
-		PRINTF(1,"(TIKIDB TESTER) Closing scope %u.\n", SCOPE_ID);
-		tikidb_process_statement(close_scope_statement_arr,
-				sizeof(close_scope_statement_arr));
+		PRINTF(1,"(TIKIDB TESTER) Closing scope %u.\n", SCOPE_ID_INSIDE_UPPER);
+		tikidb_process_statement(close_scope_statement_upper_arr,
+				sizeof(close_scope_statement_upper_arr));
+
+		static uint8_t close_scope_statement_lower_arr[] = { //
+			STATEMENT, // indicates that the byte array is a statement (instead of a result)
+			12, // statement id
+			CLOSE_SCOPE_STATEMENT, // type of statement is OPEN SCOPE (instead e.g., of STOP or LIST_QUERIES)
+			SCOPE_ID_INSIDE_LOWER // id of the scope
+		};
+
+		PRINTF(1,"(TIKIDB TESTER) Closing scope %u.\n", SCOPE_ID_INSIDE_LOWER);
+				tikidb_process_statement(close_scope_statement_lower_arr,
+						sizeof(close_scope_statement_lower_arr));
+
+		static uint8_t close_scope_statement_outer_arr[] = { //
+			STATEMENT, // indicates that the byte array is a statement (instead of a result)
+			12, // statement id
+			CLOSE_SCOPE_STATEMENT, // type of statement is OPEN SCOPE (instead e.g., of STOP or LIST_QUERIES)
+			SCOPE_ID_OUTSIDE // id of the scope
+		};
+
+		PRINTF(1,"(TIKIDB TESTER) Closing scope %u.\n", SCOPE_ID_OUTSIDE);
+				tikidb_process_statement(close_scope_statement_outer_arr,
+						sizeof(close_scope_statement_outer_arr));
 
 		PRINTF(1,"(TIKIDB TESTER) Waiting 100 seconds.\n");
 		etimer_set(&control_timer, 100 * CLOCK_SECOND);
